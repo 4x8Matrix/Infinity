14dde2fd622ef0da92feed239719aa272021f8b2
AsynchronousMatrix <catbob614@gmail.com>
2023-05-29 12:22:38 +0100

    feature: expose both 'Component' & 'Singleton' objects

--- Source/init.lua
+++ Source/init.lua
@@ -1,6 +1,29 @@
+-- // Dependencies
+local Promise = require(script.Parent.Promise)
+local Signal = require(script.Parent.Signal)
+
 -- // Module
-local State = { }
+local Infinity = { }
 
-State.Interface = { }
+Infinity.Internal = { }
+Infinity.Interface = { }
 
-return State.Interface
+Infinity.Interface.Component = require(script.Component)
+Infinity.Interface.Singleton = require(script.Singleton)
+
+Infinity.Internal.ComponentTagAdded = Signal.new()
+Infinity.Internal.ComponentTagRemoved = Signal.new()
+
+function Infinity.Interface:Start()
+	return Promise.new(function(resolve)
+		local singletons = Infinity.Interface.Singleton._getInstances()
+
+		for _, singleton in singletons do
+			singleton:InvokeLifecycle("OnStart")
+		end
+
+		resolve()
+	end)
+end
+
+return Infinity.Interface
