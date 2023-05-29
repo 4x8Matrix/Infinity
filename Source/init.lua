378105bebacaaac214609b652394fc408b0de567
AsynchronousMatrix <catbob614@gmail.com>
2023-05-29 12:37:16 +0100

    feature: implement 'timeout' watchdog

--- Source/init.lua
+++ Source/init.lua
@@ -2,6 +2,8 @@
 local Promise = require(script.Parent.Promise)
 local Signal = require(script.Parent.Signal)
 
+local DELAY_BEFORE_WARN = 5
+
 -- // Module
 local Infinity = { }
 
@@ -11,15 +13,30 @@ Infinity.Interface = { }
 Infinity.Interface.Component = require(script.Component)
 Infinity.Interface.Singleton = require(script.Singleton)
 
+Infinity.Internal.Timeout = DELAY_BEFORE_WARN
+
 Infinity.Internal.ComponentTagAdded = Signal.new()
 Infinity.Internal.ComponentTagRemoved = Signal.new()
+
+function Infinity.Interface:SetTimeout(Timeout)
+	Infinity.Internal.Timeout = Timeout
+end
 
 function Infinity.Interface:Start()
 	return Promise.new(function(resolve)
 		local singletons = Infinity.Interface.Singleton._getInstances()
+		local activeThread = coroutine.running()
 
 		for _, singleton in singletons do
+			local timeoutThread = task.delay(Infinity.Internal.Timeout, function()
+				warn(`Singleton '{tostring(singleton)}' has taken over {Infinity.Internal.Timeout} to start.\nStack Begin\n{debug.traceback(activeThread)}\nStackEnd`)
+			end)
+
+			debug.profilebegin(singleton.Name)
 			singleton:InvokeLifecycle("OnStart")
+			debug.profileend()
+
+			task.cancel(timeoutThread)
 		end
 
 		resolve()
