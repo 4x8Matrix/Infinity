d195baffec607a1377caddd8e53070766482cca2
AsynchronousMatrix <catbob614@gmail.com>
2023-05-29 12:45:08 +0100

    feature: implement 'os.clock' & 'onStarted' signal

--- Source/init.lua
+++ Source/init.lua
@@ -14,6 +14,9 @@ Infinity.Interface = { }
 Infinity.Interface.Component = require(script.Component)
 Infinity.Interface.Singleton = require(script.Singleton)
 
+Infinity.Interface.OnStarted = Signal.new()
+Infinity.Interface.Started = false
+
 Infinity.Internal.Timeout = DELAY_BEFORE_WARN
 
 Infinity.Internal.ComponentTagAdded = Signal.new()
@@ -23,12 +26,22 @@ function Infinity.Interface:SetTimeout(Timeout)
 	Infinity.Internal.Timeout = Timeout
 end
 
+function Infinity.Interface:Yield()
+	if Infinity.Interface.Started then
+		return
+	end
+
+	Infinity.Interface.OnStarted:Wait()
+end
+
 function Infinity.Interface:Start()
 	return Promise.new(function(resolve)
 		local singletons = Infinity.Interface.Singleton._getInstances()
+		local singletonClocks = { }
 		local activeThread = coroutine.running()
 
 		for _, singleton in singletons do
+			local singletonDelta = os.clock()
 			local timeoutThread = task.delay(Infinity.Internal.Timeout, function()
 				warn(`Singleton '{tostring(singleton)}' has taken over {Infinity.Internal.Timeout} to start.\nStack Begin\n{debug.traceback(activeThread)}\nStackEnd`)
 			end)
@@ -37,10 +50,15 @@ function Infinity.Interface:Start()
 			singleton:InvokeLifecycle("OnStart")
 			debug.profileend()
 
+			singletonClocks[singleton] = os.clock() - singletonDelta
+
 			task.cancel(timeoutThread)
 		end
 
-		resolve()
+		Infinity.Interface.Started = true
+		Infinity.Interface.OnStarted:Fire(singletonClocks)
+
+		resolve(singletonClocks)
 	end)
 end
 
